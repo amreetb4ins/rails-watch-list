370224ceff2b2508f9f1c157f1b2bf4467785c0d
Amreet Bains <amreet8@hotmail.co.uk>
2024-09-26 18:02:11 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "lists", to: "lists#index"
+  resources :lists, except: [ :edit, :update ] do
+    resources :bookmarks, only: [ :create, :new ]
+  end
 end
